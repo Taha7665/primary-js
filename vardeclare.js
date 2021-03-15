f7fdc1065d1495a98831d5f113a10d8a83dd0ed9
Papeya-taahaa <Taha7665>
2021-03-15 09:34:33 +0600

    first commit

---
+++ vardeclare.js
@@ -0,0 +1,13 @@
+var planet = "neptune";
+var anotherPlanet = "Mars";
+var nextPlanet = "jupiter";
+console.log(anotherPlanet);
+console.log(planet , anotherPlanet , nextPlanet);
+
+
+// js variable case sensitive
+
+var age = "22";
+var AGE = "13";
+console.log(AGE);
+
